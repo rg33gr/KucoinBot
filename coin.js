672d17b38b2ba99ec0ff205522388a6585dbf2e3
Kobe Wilson <kobe.wilson81@gmail.com>
2021-08-10 21:40:01 -0400

    upload project to git

---
+++ coin.js
@@ -0,0 +1,7 @@
+class Coin {
+    constructor(coin){
+        this.symbol = coin.symbol;
+    }
+}
+
+module.exports = Coin;
---
+++ output/coin.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Coin = void 0;
+var Coin = /** @class */ (function () {
+    function Coin(symbol) {
+        this.symbol = symbol;
+    }
+    return Coin;
+}());
+exports.Coin = Coin;
