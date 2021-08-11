06f7761fb2659e927eb7ca6fd11a120e62db30e9
Kobe Wilson <kobe.wilson81@gmail.com>
2021-08-10 21:30:53 -0400

    enter

---
+++ app.js
@@ -0,0 +1,77 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const axios = require('axios')
+const Coin = require('./coin.js')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+const url = "https://api.kucoin.com/api/v1/symbols"
+const usdtPairs = [];
+
+const getData = async (url) => {
+
+  try {
+    const response = await axios.get(url)
+    const data = response.data.data // Array of all sybomls
+    
+    for (let coin in data){
+      if (data[coin].symbol.includes("USDT")){
+        usdtPairs.push(data[coin])
+      }
+       //console.log(data[coin].symbol)
+    }
+
+    console.log("Number of coins is: " + usdtPairs.length)
+
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+
+getData(url);
+
+var firstCoin = new Coin(usdtPairs[0])
+console.log("THE FIRST COIN IS: " + firstCoin.symbol);
+
+
+
+module.exports = app;
